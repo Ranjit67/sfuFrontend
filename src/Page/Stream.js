950935eef881203effa69b2bc11a9f440da9a359
Ranjit67 <ranjitsahoo873@gmail.com>
2021-08-08 15:45:08 +0530

    update sfu

---
+++ src/Page/Stream.js
@@ -0,0 +1,248 @@
+import {
+  useEffect,
+  // useState,
+  useRef,
+} from "react";
+import { io } from "socket.io-client";
+
+export default function Stream() {
+  // const [latestStream, setLatestStream] = useState();
+  // useEffect(() => {
+  //   wholeProcess();
+  //   return () => {
+  //     // setLatestStream();
+  //   };
+  // }, []);
+  const myVideo = useRef();
+  const socketRef = useRef();
+  const pc = useRef();
+  const socketFn = () => {
+    // socketRef.current = io.connect("http://localhost:9000/stream");
+    socketRef.current = io.connect("https://sfuvideo.herokuapp.com/stream");
+    navigator.mediaDevices
+      .getUserMedia({
+        video: true,
+        audio: true,
+      })
+      .then(async (stream) => {
+        myVideo.current.srcObject = stream;
+        pc.current = await createPeer();
+        stream
+          .getTracks()
+          .forEach((track) => pc.current.addTrack(track, stream));
+        const offer = await pc.current.createOffer();
+        pc.current.setLocalDescription(offer).then((stag) => {
+          sendMessage("offer", { offer: pc.current.localDescription });
+        });
+        socketRef.current.on("answer", async (payload) => {
+          const { answer } = payload;
+          const desc = new RTCSessionDescription(answer);
+          await pc.current
+            .setRemoteDescription(desc)
+            .catch((error) => console.log(error));
+        });
+
+        if (pc.current) {
+          console.log(pc.current);
+
+          pc.current.onicecandidate = (event) => {
+            if (event.candidate) {
+              socketRef.current.emit("ice_stream", { ice: event.candidate });
+            }
+          };
+          // events
+          pc.current.onconnectionstatechange = (event) => {
+            console.log("===0***===");
+            console.log("onconnectionstatechange", event);
+            console.log(
+              "pc.current.connectionState",
+              pc.current?.connectionState
+            );
+            console.log("===0***===");
+          };
+          pc.current.oniceconnectionstatechange = async (event) => {
+            if (pc.current?.iceConnectionState === "failed") {
+              await pc.current.restartIce();
+              console.log("hit restart");
+            }
+
+            console.log("===1***===");
+            console.log("oniceconnectionstatechange ", event);
+            console.log(
+              "pc.current.iceConnectionState ",
+              pc.current?.iceConnectionState
+            );
+            console.log("===1***===");
+          };
+          pc.current.onicegatheringstatechange = (event) => {
+            console.log("===2***===");
+            console.log("onicegatheringstatechange ", event);
+            console.log(
+              "pc.current.iceGatheringState",
+              pc.current?.iceGatheringState
+            );
+            console.log("===2***===");
+          };
+          // events end
+        }
+        // backend event
+        socketRef.current.on("event", (payload) => {
+          console.log(payload);
+        });
+
+        // backend event end
+        socketRef.current.on("ice", async (payload) => {
+          const { ice } = payload;
+          // console.log("other", payload);
+          if (ice) {
+            await pc.current.addIceCandidate(ice);
+          }
+        });
+
+        //then end
+      });
+
+    //function end
+  };
+
+  const sendMessage = (message, value) => {
+    socketRef.current.emit(message, value);
+  };
+  const wholeProcess = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+    myVideo.current.srcObject = stream;
+    const peer = await createPeer();
+    stream.getTracks().forEach((track) => peer.addTrack(track, stream));
+    console.log(peer);
+  };
+  const createPeer = async () => {
+    const peer = new RTCPeerConnection({
+      iceServers: [
+        {
+          urls: "stun:stun.stunprotocol.org",
+        },
+        { urls: "stun:stun.ekiga.net" },
+        { urls: "stun:stun.schlund.de" },
+        { urls: "stun:stun.l.google.com:19302" },
+
+        { urls: "stun:stun1.l.google.com:19302" },
+        { urls: "stun:stun2.l.google.com:19302" },
+        { urls: "stun:stun3.l.google.com:19302" },
+        { urls: "stun:stun4.l.google.com:19302" },
+        { urls: "stun:stun.softjoys.com" },
+        { urls: "stun:stun.voipbuster.com" },
+        { urls: "stun:stun.voipstunt.com" },
+        { urls: "stun:stun.xten.com" },
+        {
+          urls: "turn:numb.viagenie.ca",
+          credential: "muazkh",
+          username: "webrtc@live.com",
+        },
+        {
+          urls: "turn:192.158.29.39:3478?transport=udp",
+          credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+          username: "28224511:1379330808",
+        },
+        {
+          urls: "turn:192.158.29.39:3478?transport=tcp",
+          credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+          username: "28224511:1379330808",
+        },
+        // {
+        //   urls: "turn:numb.viagenie.ca",
+        //   credential: "muazkh",
+        //   username: "webrtc@live.com",
+        // },
+      ],
+    });
+    // peer.onnegotiationneeded = () => handleNegotiationNeededEvent(peer);
+    return peer;
+  };
+  const handleNegotiationNeededEvent = async (peer) => {
+    const offer = await peer.createOffer();
+    await peer.setLocalDescription(offer);
+
+    await fetchFunction(peer.localDescription, peer);
+    // console.log(offer);
+  };
+  const fetchFunction = async (sdp, peer) => {
+    try {
+      //http://sfuvideo.herokuapp.com/broadcast
+      //http://localhost:9000/broadcast
+      const response = await fetch("http://sfuvideo.herokuapp.com/broadcast", {
+        method: "POST",
+        body: JSON.stringify({
+          sdp,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
+      const result = await response.json();
+      // console.log(result.sdp);
+      const desc = new RTCSessionDescription(result.sdp);
+      peer.setRemoteDescription(desc).catch((error) => console.log(error));
+      peer.onicecandidate = (event) => {
+        if (event.candidate) {
+          //
+          iceFetch(event.candidate, peer);
+        } else {
+          console.log("end");
+        }
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const iceFetch = async (ice, peer) => {
+    const response2 = await fetch("http://sfuvideo.herokuapp.com/addIce", {
+      method: "POST",
+      body: JSON.stringify({
+        ice: ice,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+    const result2 = await response2.json();
+    //
+    result2.ice?.forEach(async (iceOther) => {
+      await peer.addIceCandidate(iceOther);
+    });
+    peer.oniceconnectionstatechange = (ev) => {
+      console.log(ev);
+    };
+    // peer.onicegatheringstatechange = function () {
+    //   // let label = "Unknown";
+    //   switch (peer.iceGatheringState) {
+    //     case "new":
+    //       console.log("new");
+
+    //       break;
+    //     case "complete":
+    //       console.log("complete");
+    //       break;
+    //     case "gathering":
+    //       console.log("gathering");
+    //       break;
+
+    //     default:
+    //       break;
+    //   }
+    // };
+    // console.log(result2.ice);
+  };
+  useEffect(() => {
+    return () => {};
+  }, []);
+  return (
+    <div>
+      {/* <p>Stream</p> */}
+      <button onClick={socketFn}>Stream</button>
+      <video ref={myVideo} muted autoPlay />
+    </div>
+  );
+}
